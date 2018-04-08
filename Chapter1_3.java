fe5909779a73dad61ca688847dfafb96ff72e5ac
Xingo <398303159@qq.com>
2018-04-08 22:45:34 +0800

    Delete Chapter1_3.java

--- Chapter1_3.java
+++
@@ -1,9 +0,0 @@
-public class Chapter1_3 {
-    public static void main(String[] args) {
-        System.out.println("    J     A     V     V    A     \n" +
-                "    J    A A     V   V    A A    \n" +
-                "J   J   AAAAA     V V    AAAAA   \n" +
-                " J J   A     A     V    A     A  ");
-
-    }
-}
